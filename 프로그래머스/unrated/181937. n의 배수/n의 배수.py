740e0c930c72f42a7739fc856a1e24015a01282c
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-08-23 01:42:06 +0900

    [unrated] Title: n의 배수, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181937. n의 배수/n의 배수.py
+++ 프로그래머스/unrated/181937. n의 배수/n의 배수.py
@@ -2,3 +2,7 @@ def solution(num, n):
     if num % n == 0:
         return 1
     return 0
+    
+    # 짝수일 경우 num & n이 0이 되고, not을 통해 true가 되고 int(true)는 1
+    # 홀수일 경우 num % n이 정수가 되고, not을 통해 false가 되고 int(false)는 0
+    # return int(not(num % n))
