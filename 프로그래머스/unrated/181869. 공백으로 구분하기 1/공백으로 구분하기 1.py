0f0ef2f13c949d0b32bb839933cb79a7af703452
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-08-23 01:44:58 +0900

    [unrated] Title: 공백으로 구분하기 1, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181869. 공백으로 구분하기 1/공백으로 구분하기 1.py
@@ -0,0 +1,10 @@
+def solution(my_string):
+    return my_string.split()
+
+# split() 메서드는 문자열을 지정한 구분자를 기준으로 나눠 '리스트로 반환'
+# 기본적으로 공백을 구분자로 사용해서 문자열을 단어로 분할함.
+'''
+string = "apple,banana,cherry"
+result = string.split(',')
+-> ['apple', 'banana', 'cherry']
+'''
