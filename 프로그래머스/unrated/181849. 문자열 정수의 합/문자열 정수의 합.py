1344aba54cf84f8ed0ef2359c954f342ffc06ef5
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-08-23 00:08:58 +0900

    Update 문자열 정수의 합.py

--- 프로그래머스/unrated/181849. 문자열 정수의 합/문자열 정수의 합.py
+++ 프로그래머스/unrated/181849. 문자열 정수의 합/문자열 정수의 합.py
@@ -3,3 +3,19 @@ def solution(num_str):
     for num in num_str:
         result += int(num)
     return result
+
+'''
+# 숫자로 된 문자열을 정수로 변환하는 함수
+def str_to_int(s):
+    return int(s)
+
+# 숫자로 된 문자열 리스트
+num_str_list = ["1", "2", "3", "4", "5"]
+
+# map()을 사용하여 각 문자열을 정수로 변환
+int_list = list(map(str_to_int, num_str_list))
+
+# 결과 출력
+print(int_list)
+
+'''
