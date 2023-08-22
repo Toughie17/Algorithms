d3fc4307f8069d3fdd28dcdb864afc6ea96c6fac
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-08-22 23:42:30 +0900

    Update 조건에 맞게 수열 변환하기 3.py

--- 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.py
+++ 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.py
@@ -3,3 +3,7 @@ def solution(arr, k):
         return [num * k for num in arr]
     else:
         return [num + k for num in arr]
+        
+# new_list = [expression for item in iterable if condition]
+# even_numbers = [x for x in range(1, 11) if x % 2 == 0]
+# iterable: 반복 가능한(iterable) 객체, 예를 들어 리스트, 튜플, 범위(range), 문자열 등이 될 수 있습니다.
