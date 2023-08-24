e0ad0ee53531cddb9d6bbd491f404b8f93f98e2d
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-08-24 23:58:11 +0900

    [unrated] Title: 원하는 문자열 찾기, Time: 0.07 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.py
+++ 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.py
@@ -1,4 +1,6 @@
 def solution(myString, pat):
-    if pat.lower() in myString.lower():
-        return 1
-    return 0
+    # if pat.lower() in myString.lower():
+    #     return 1
+    # return 0
+
+    return int(pat.lower() in myString.lower())
