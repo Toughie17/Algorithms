83c5a7b8ea80b6b3bd6815b10bb9ae4e5a86ecc9
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-08-25 00:30:07 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 1, Time: 10.28 ms, Memory: 17.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.py
+++ 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.py
@@ -9,11 +9,16 @@ def solution(arr):
             
 #     return answer
 #위는 처음 작성 코드
-    answer = arr.copy()
-    for index, value in enumerate(answer):
-        if value >= 50 and value % 2 == 0:
-            answer[index] /= 2
-        elif value < 50 and value % 2 == 1:
-            answer[index] *= 2
-    return answer
+    # answer = arr.copy()
+    # for index, value in enumerate(answer):
+    #     if value >= 50 and value % 2 == 0:
+    #         answer[index] /= 2
+    #     elif value < 50 and value % 2 == 1:
+    #         answer[index] *= 2
+    # return answer
 #enumerate 사용한 방법
+
+#list complehension 사용
+    return [x / 2 if x >= 50 and x % 2 == 0 
+            else (x * 2 if x <50 and x % 2 == 1 else x)
+            for x in arr]
