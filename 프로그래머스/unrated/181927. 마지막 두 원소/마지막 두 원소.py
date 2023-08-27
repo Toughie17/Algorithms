bdaf88136e3994a27069b328b3f6ead53da7e29f
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-08-28 01:47:40 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.py
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.py
@@ -1,12 +1,18 @@
 def solution(num_list):
-    last_value = num_list[-1]
-    before_value = num_list[-2]
+#     last_value = num_list[-1]
+#     before_value = num_list[-2]
     
-    if last_value > before_value:
-        num_list.append(last_value - before_value)
-    else:
-        num_list.append(last_value * 2)
+#     if last_value > before_value:
+#         num_list.append(last_value - before_value)
+#     else:
+#         num_list.append(last_value * 2)
     
-    return num_list
+#     return num_list
 
 # 배열 슬라이싱 활용
+
+ #조건문 활용 더 짧은 답안
+    # .append(넣을 요소, 조건문, 아닌 경우 넣을 요소)
+    num_list.append((num_list[-1] - num_list[-2]) if num_list[-1] > num_list[-2] else num_list[-1] * 2 )
+    
+    return num_list
