fab7cbc51f8e151f29fd1513f23aec68226ac176
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-08-28 00:50:36 +0900

    [unrated] Title: 접두사인지 확인하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.py
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.py
@@ -1,5 +1,11 @@
 def solution(my_string, is_prefix):
     # return int(is_prefix in my_string)
-    return int(my_string.startswith(is_prefix))
+    # return int(my_string.startswith(is_prefix))
 
 # 특정 접두사로 시작하는 지 검사하는 데 사용되는 문자열 메서드 startswith
+
+# 만약 메서드를 몰랐다면
+
+    if my_string[:len(is_prefix)] == is_prefix:
+        return 1
+    return 0
