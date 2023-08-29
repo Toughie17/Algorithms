6667aa6d8f10bb3e30b8d170245ad01da73d8057
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-08-29 20:48:27 +0900

    [unrated] Title: 접미사인지 확인하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.py
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.py
@@ -1,4 +1,8 @@
 def solution(my_string, is_suffix):
-    if my_string.endswith(is_suffix):
+    # if my_string.endswith(is_suffix):
+    #     return 1
+    # return 0
+
+    if my_string[-len(is_suffix):] == is_suffix:
         return 1
     return 0
