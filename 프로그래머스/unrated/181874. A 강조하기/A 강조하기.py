2d779259c15c54d8c26c629ed69b5f3db810d423
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-08-29 17:39:38 +0900

    [unrated] Title: A 강조하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.py
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.py
@@ -1,11 +1,14 @@
 def solution(myString):
-    answer = ''
+#     answer = ''
     
-    for cha in myString:
-        if cha == 'a':
-            answer += "A"
-        elif cha != "A" and cha.isupper():
-                answer += cha.lower()
-        else: answer += cha
+#     for cha in myString:
+#         if cha == 'a':
+#             answer += "A"
+#         elif cha != "A" and cha.isupper():
+#                 answer += cha.lower()
+#         else: answer += cha
                 
-    return answer
+#     return answer
+
+    # 결국 대문자는 A만 남기고 나머지는 다 소문자로 바꾸겠다는 말이니
+    return myString.lower().replace('a', 'A')
