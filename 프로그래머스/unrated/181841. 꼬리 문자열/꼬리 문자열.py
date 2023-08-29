44fa1070bdb1041730fe623042111b617620566f
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-08-29 20:53:25 +0900

    [unrated] Title: 꼬리 문자열, Time: 0.00 ms, Memory: 9.99 MB -BaekjoonHub

--- 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.py
+++ 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.py
@@ -1,6 +1,7 @@
 def solution(str_list, ex):
-    result = ""
-    for str in str_list:
-        if ex not in str:
-            result += str
-    return result
+    # result = ""
+    # for str in str_list:
+    #     if ex not in str:
+    #         result += str
+            
+    return ''.join([x for x in str_list if ex not in x])
