575c70bc9fbf62f38afde77dd42cb3e606a03de3
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-08-29 20:40:27 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.py
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.py
@@ -1,3 +1,6 @@
 def solution(num_list):
     # return sorted(num_list, reverse = True) # 실패. 정렬이 아니라 뒤집기만 하도록
-    return num_list[::-1]
+    # return num_list[::-1]
+    
+    # reverse 사용
+    return list(reversed(num_list))
