ddb7d3e66a6d91fa4b2b98abc6758fa649e7345c
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-08-29 21:13:57 +0900

    [unrated] Title: 더 크게 합치기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,11 +1,14 @@
 def solution(a, b):
-    left = int(str(a) + str(b))
-    right = int(str(b) + str(a))
+#     left = int(str(a) + str(b))
+#     right = int(str(b) + str(a))
     
-    if left > right:
-        return left
-    elif right > left:
-        return right
-    else:
-        return left
+#     if left > right:
+#         return left
+#     elif right > left:
+#         return right
+#     else:
+#         return left
+    
+    #다른 풀이
+    return max(map(int,(f"{a}{b}", f"{b}{a}")))
     
