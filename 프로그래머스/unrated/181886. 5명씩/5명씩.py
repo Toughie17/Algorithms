1cceab3aed244a9f840e5c7bd76f690378054cd1
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-08-29 16:29:18 +0900

    [unrated] Title: 5명씩, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181886. 5명씩/5명씩.py
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.py
@@ -1,12 +1,16 @@
 def solution(names):
     # 나눈 배열을 담을 배열(2차원)   
-    divided_arr = []
+#     divided_arr = []
     
-    # 5개씩 나눠서 divided_arr에 추가
-    for i in range(0, len(names), 5):
-        part = names[i: i+5]
-        divided_arr.append(part)
+#     # 5개씩 나눠서 divided_arr에 추가
+#     for i in range(0, len(names), 5):
+#         part = names[i: i+5]
+#         divided_arr.append(part)
     
-    # divided_arr의 각 요소의 첫번째 요소 뽑기
-    return [ part[0] for part in divided_arr ]
+#     # divided_arr의 각 요소의 첫번째 요소 뽑기
+#     return [ part[0] for part in divided_arr ]
     
+    
+    # 엄청 간단한 풀이 
+    # 결국 처음부터 5개 간격으로 요소를 확인하는 것..
+    return names[::5]
