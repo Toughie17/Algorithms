5db8c5b7210e22884826ee3a6df66a42129fe9c4
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-02 19:41:50 +0900

    [unrated] Title: 할 일 목록, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.py
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.py
@@ -1,8 +1,10 @@
 def solution(todo_list, finished):
-    left = []
+#     left = []
         
-    for index, todo in enumerate(todo_list):
-        if not finished[index]:
-            left.append(todo)
+#     for index, todo in enumerate(todo_list):
+#         if not finished[index]:
+#             left.append(todo)
             
-    return left
+#     return left
+
+    return [work for idx, work in enumerate(todo_list) if not finished[idx] ]
