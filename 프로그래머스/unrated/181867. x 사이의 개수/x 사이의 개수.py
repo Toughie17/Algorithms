2bc5493e20f8e0549880bba6d09ec7eaaaeebe30
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-02 22:28:26 +0900

    [unrated] Title: x 사이의 개수, Time: 1.67 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.py
+++ 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.py
@@ -1,6 +1,8 @@
 def solution(myString):
-    result = []
+#     result = []
     
-    for x in myString.split('x'):
-        result.append(len(x))
-    return result
+#     for x in myString.split('x'):
+#         result.append(len(x))
+#     return result
+
+    return [len(x) for x in myString.split('x')]
