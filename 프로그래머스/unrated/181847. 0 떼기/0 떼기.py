b2266dbf649aee3e4ca77f93593fe233834a9a58
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-02 19:11:36 +0900

    [unrated] Title: 0 떼기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181847. 0 떼기/0 떼기.py
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.py
@@ -1,5 +1,8 @@
 def solution(n_str):
     
-    for i in range(len(n_str)):
-        if n_str[i] != '0':
-            return n_str[i:]
+    # for i in range(len(n_str)):
+    #     if n_str[i] != '0':
+    #         return n_str[i:]
+    
+    #간단한 풀이
+    return n_str.lstrip('0')
