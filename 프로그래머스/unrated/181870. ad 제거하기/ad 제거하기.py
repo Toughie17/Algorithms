3f6e732d6e745c2d718af19c04907271b236d6d4
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-02 23:23:10 +0900

    [unrated] Title: ad 제거하기, Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.py
@@ -0,0 +1,8 @@
+def solution(strArr):
+    # result = []
+    # for str in strArr:
+    #     if 'ad' not in str:
+    #         result.append(str)
+    # return result
+
+    return [str for str in strArr if 'ad' not in str]
