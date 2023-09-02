a30b27055ecff323ad6334ab363addf9f353967b
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-02 23:39:25 +0900

    [unrated] Title: 간단한 식 계산하기, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.py
@@ -0,0 +1,14 @@
+def solution(binomial):
+    a, op, b = binomial.split()
+    num1, num2 = int(a), int(b)
+    
+    # if op == '+':
+    #     return num1 + num2
+    # elif op == '-':
+    #     return num1 - num2
+    # else:
+    #     return num1 * num2
+
+    operators = {'+': num1 + num2, '-': num1 - num2, '*': num1 * num2}
+    if op in operators:
+        return operators[op]
