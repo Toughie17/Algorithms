207424af89d4be8b60d64e3b3db4aa0fe7489475
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-02 19:16:42 +0900

    [unrated] Title: l로 만들기, Time: 6.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181834. l로 만들기/l로 만들기.py
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.py
@@ -1,12 +1,16 @@
 def solution(myString):
     #l보다 앞인지 뒤인지 어떻게 판단하지
     #문자열 직접 비교가 가능하네..
-    result = ''
+#     result = ''
     
-    for cha in myString:
-        if cha < 'l':
-            result += 'l'
-        else:
-            result += cha
+#     for cha in myString:
+#         if cha < 'l':
+#             result += 'l'
+#         else:
+#             result += cha
             
-    return result
+#     return result
+
+    #더 짧은 풀이
+    answer = [x if x > 'l' else 'l' for x in myString]
+    return ''.join(answer)
