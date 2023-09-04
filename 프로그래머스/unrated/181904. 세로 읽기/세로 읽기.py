1278f3485b1027809fae57923ee75403fa602da8
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-04 22:12:56 +0900

    [unrated] Title: 세로 읽기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.py
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.py
@@ -1,6 +1,8 @@
 def solution(my_string,m,c):
-    result = []
-    #결국은 c번째 문자부터 m간격으로 떨어진 애들의 조합임.
-    for i in range(c-1, len(my_string), m):
-        result.append(my_string[i])
-    return ''.join(result)
+    # result = []
+    # #결국은 c번째 문자부터 m간격으로 떨어진 애들의 조합임.
+    # for i in range(c-1, len(my_string), m):
+    #     result.append(my_string[i])
+    # return ''.join(result)
+
+    return my_string[c-1::m]
