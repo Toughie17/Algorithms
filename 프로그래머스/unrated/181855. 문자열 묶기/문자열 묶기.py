251b3c266c0b5966e3db7b2988c09654af5709ca
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-05 23:09:11 +0900

    [unrated] Title: 문자열 묶기, Time: 14.92 ms, Memory: 17.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.py
+++ 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.py
@@ -1,11 +1,16 @@
-from collections import Counter
+# from collections import Counter
 
 def solution(strArr):
-    sol = []
-    for str in strArr:
-        sol.append(len(str))
+#     sol = []
+#     for str in strArr:
+#         sol.append(len(str))
         
-    counts = Counter(sol)
-    # Counter({길이1: 빈도수1, 길이2: 빈도수2, ...})
+#     counts = Counter(sol)
+#     # Counter({길이1: 빈도수1, 길이2: 빈도수2, ...})
     
-    return max(counts.values())
+#     return max(counts.values())
+    dic = {}
+    
+    for str in strArr:
+        dic[len(str)] = dic.get(len(str), 0) + 1
+    return max(dic.values())
