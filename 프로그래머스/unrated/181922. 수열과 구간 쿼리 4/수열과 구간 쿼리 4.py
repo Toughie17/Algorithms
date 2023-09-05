7f8f1d6efaa1c1232cbbc2ad7c4b8346f053bc7d
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-05 20:40:33 +0900

    [unrated] Title: 수열과 구간 쿼리 4, Time: 4.92 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.py
+++ 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.py
@@ -1,8 +1,7 @@
 def solution(arr, queries):
     
-    for query in queries:
-        for i in range(query[0], query[1] + 1):
-            #i가 k의 배수이면
-            if i % query[2] == 0:
+    for (s, e, k) in queries:
+        for i in range(s, e + 1):
+            if i % k == 0:
                 arr[i] += 1
     return arr
