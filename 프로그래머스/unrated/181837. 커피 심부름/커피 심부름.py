2667d25d66d475651dd8c0197d0cb053a278cfe6
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-05 23:21:56 +0900

    [unrated] Title: 커피 심부름, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.py
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.py
@@ -2,13 +2,21 @@ def solution(order):
     # 메뉴만 적은 팀원의 것은 차가운 것으로 통일
     # 아무거나는 차가운 아메리카노
     # 라떼 아니면 다 아아임
-    aa = 0
-    cl = 0
+#     aa = 0
+#     cl = 0
     
+#     for menu in order:
+#         if 'latte' in menu:
+#             cl += 5000
+#         else:
+#             aa += 4500
+            
+#     return (aa + cl)
+
+    #라떼만 500원 더 비쌈.
+    answer = 0
     for menu in order:
         if 'latte' in menu:
-            cl += 5000
-        else:
-            aa += 4500
-            
-    return (aa + cl)
+            answer += 500
+        answer += 4500
+    return answer
