5f3086975131ce0f90f9c7bb369dab3098757102
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-05 21:47:33 +0900

    [unrated] Title: 2의 영역, Time: 3.45 ms, Memory: 15.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181894. 2의 영역/2의 영역.py
+++ 프로그래머스/unrated/181894. 2의 영역/2의 영역.py
@@ -6,5 +6,7 @@ def solution(arr):
             indexs.append(index)
     if indexs:
         return arr[indexs[0] : indexs.pop() + 1]
+    return [ -1]
     
-    return [-1]
+    # return [-1] if 2 not in arr else arr[arr.index(2): len(arr) - arr[::-1].index(2) ]
+
