34d467f3ac494dd31b614f01bb915a75fa7683bf
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-07 16:11:06 +0900

    [unrated] Title: 문자 개수 세기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.py
@@ -0,0 +1,14 @@
+# ord() 함수는 문자(character)를 해당하는 유니코드 코드 포인트(정수)로 변환하는 파이썬 내장 함수
+
+def solution(my_string):
+    counts = [0] * 52
+    
+    for cha in my_string:
+        #0번부터 25번까지 대문자 26개
+        if 'A' <= cha <= 'Z':
+            counts[ord(cha) - ord('A')] += 1
+        #26번부터 51번까지 소문자 26개
+        elif 'a' <= cha <= 'z':
+            counts[ord(cha) - ord('a') + 26] += 1
+    
+    return counts
