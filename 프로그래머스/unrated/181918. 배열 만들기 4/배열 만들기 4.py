3ff1bf1297a0b44d9e06354188c2ff67b6c06888
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-10 20:38:16 +0900

    [unrated] Title: 배열 만들기 4, Time: 3.40 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.py
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.py
@@ -1,17 +1,24 @@
 def solution(arr):
-    i = 0
+#     i = 0
+#     stk = []
+    
+#     while i < len(arr):
+#         if len(stk) == 0:
+#             stk.append(arr[i])
+#             i += 1
+            
+#         elif stk[-1] < arr[i]:
+#             stk.append(arr[i])
+#             i += 1
+            
+#         elif stk[-1] >= arr[i]:
+#             stk.pop()
+    
+#     return stk
+
     stk = []
-    
-    while i < len(arr):
-        if len(stk) == 0:
-            stk.append(arr[i])
-            i += 1
-            
-        elif stk[-1] < arr[i]:
-            stk.append(arr[i])
-            i += 1
-            
-        elif stk[-1] >= arr[i]:
+    for i in range(len(arr)):
+        while stk and stk[-1] >= arr[i]:
             stk.pop()
-    
+        stk.append(arr[i])
     return stk
