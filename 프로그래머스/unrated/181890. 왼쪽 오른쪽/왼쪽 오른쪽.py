6a957798f55241bfcc5c7b4f7aff40d10a606c32
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-10 20:50:23 +0900

    [unrated] Title: 왼쪽 오른쪽, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
@@ -0,0 +1,12 @@
+def solution(str_list):
+    
+    for index, cha in enumerate(str_list):
+        if cha == "l":
+            return str_list[:index]
+        elif cha == 'r':
+            return str_list[index+ 1:]
+        else:
+            continue
+            
+    return []
+    
