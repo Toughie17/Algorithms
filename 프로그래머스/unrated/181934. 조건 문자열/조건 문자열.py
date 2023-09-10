79afe843e819a38e98da56e46c665b420aa6fc04
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-10 21:24:53 +0900

    [unrated] Title: 조건 문자열, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.py
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.py
@@ -1,11 +1,15 @@
 def solution(ineq, eq, n, m):
-    if (ineq == '>' and eq == '=' and n >= m):
-        return 1
-    elif (ineq == '<' and eq == '=' and n <= m):
-        return 1
-    elif (ineq == '>' and eq == '!' and n > m):
-        return 1
-    elif (ineq == '<' and eq == '!' and n < m):
-        return 1
+#     if (ineq == '>' and eq == '=' and n >= m):
+#         return 1
+#     elif (ineq == '<' and eq == '=' and n <= m):
+#         return 1
+#     elif (ineq == '>' and eq == '!' and n > m):
+#         return 1
+#     elif (ineq == '<' and eq == '!' and n < m):
+#         return 1
     
-    return 0
+#     return 0
+
+    # 평가식을 이용하는 방법
+    # eval은 파이썬에서 문자열로 표현된 파이썬 코드를 실행하는 내장 함수
+    return int(eval((str(n)+ineq+eq).replace('!', '')+ str(m)))
