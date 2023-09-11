845b0d8f8445b27cf6a9b9c7dfafba8a6677f90d
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-12 02:47:25 +0900

    Update 정사각형으로 만들기.py

--- 프로그래머스/unrated/181830. 정사각형으로 만들기/정사각형으로 만들기.py
+++ 프로그래머스/unrated/181830. 정사각형으로 만들기/정사각형으로 만들기.py
@@ -1,44 +1,13 @@
-# def solution(arr):
-#     row_len = len(arr)
-#     col_len = len(arr[0])
-    
-#     if row_len > col_len:
-#         for i in range(len(arr)):
-#             arr[i].append(0 * (col_len - row_len))
-    
-#     elif col_len > row_len:
-#         for _ in range(col_len - row_len):
-#             arr.append([0] * (col_len))
-    
-#     return arr
-
-# def solution(arr):
-#     answer = []
-    
-#     row = len(arr)
-#     col = len(arr[0])
-    
-#     if row > col:
-#         for i in arr:
-#             answer.append(i + [0] * (row - col))
-#     elif row < col:
-#         for _ in range(col - row):
-#             arr.append([0] * col)
-#         answer = arr
-#     else:
-#         answer = arr
-    
-#     return answer
-
 def solution(arr):
-    num_rows = len(arr)
-    num_cols = len(arr[0])
-
-    if num_rows > num_cols:
-        for i in range(num_rows):
-            arr[i] += [0] * (num_rows - num_cols)
-    elif num_cols > num_rows:
-        for i in range(num_cols - num_rows):
-            arr.append([0] * num_cols)
-
+    
+    row_len = len(arr)
+    col_len = len(arr[0])
+    
+    if row_len > col_len:
+        for i in range(len(arr)):
+            arr[i].extend([0] * (row_len - col_len))
+    elif row_len < col_len:
+        for _ in range(col_len - row_len):
+            arr.append([0] * col_len)
+    
     return arr
