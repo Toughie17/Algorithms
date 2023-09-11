9fde46fef4380292488bc1c5bdee759f685236e2
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-12 01:49:27 +0900

    [unrated] Title: 무작위로 K개의 수 뽑기, Time: 2092.80 ms, Memory: 11 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.py
@@ -0,0 +1,12 @@
+def solution(arr, k):
+    answer = []
+
+    for num in arr:
+        if num not in answer:
+            answer.append(num)
+    
+    if len(answer) > k:
+        return answer[:k]
+    else:
+        answer.extend([-1] * (k - len(answer)))
+        return answer
