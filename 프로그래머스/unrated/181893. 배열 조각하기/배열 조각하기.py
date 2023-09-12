76fe889e28c9649f2f160171fad04d731f425d5c
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-13 01:27:21 +0900

    [unrated] Title: 배열 조각하기, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.py
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.py
@@ -1,9 +1,16 @@
 def solution(arr, query):
     
-    for i in range(len(query)):
-        if i % 2 == 0:
-            arr = arr[:query[i] + 1]
-        elif i % 2 == 1:
-            arr = arr[query[i]:]
+#     for i in range(len(query)):
+#         if i % 2 == 0:
+#             arr = arr[:query[i] + 1]
+#         elif i % 2 == 1:
+#             arr = arr[query[i]:]
             
+#     return arr
+
+    for (index, query) in enumerate(query):
+        if index % 2 == 0:
+            arr = arr[:query + 1]
+        elif index % 2 == 1:
+            arr = arr[query:]
     return arr
