6ac71569c4e0c595ef3514ebe0c40b83c2484134
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-13 01:14:06 +0900

    [unrated] Title: 코드 처리하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.py
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.py
@@ -1,18 +1,21 @@
 def solution(code):
-    mode = 0
-    ret = []
+#     mode = 0
+#     ret = []
     
-    for i in range(len(code)):
-        if mode == 0:
-            if code[i] != '1' and i % 2 == 0:
-                ret.append(code[i])
-            elif code[i] == '1':
-                mode = 1
+#     for i in range(len(code)):
+#         if mode == 0:
+#             if code[i] != '1' and i % 2 == 0:
+#                 ret.append(code[i])
+#             elif code[i] == '1':
+#                 mode = 1
         
-        elif mode == 1:
-            if code[i] != '1' and i % 2 == 1:
-                ret.append(code[i])
-            elif code[i] == '1':
-                mode = 0
+#         elif mode == 1:
+#             if code[i] != '1' and i % 2 == 1:
+#                 ret.append(code[i])
+#             elif code[i] == '1':
+#                 mode = 0
     
-    return (''.join(ret) if ret else "EMPTY")
+#     return (''.join(ret) if ret else "EMPTY")
+
+    # ;;;;
+    return ''.join(code.split('1'))[::2] or "EMPTY"
