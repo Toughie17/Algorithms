b8d5f1388fa8c265bc0faf83c3170f267629ba3a
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-12 23:03:14 +0900

    [unrated] Title: 그림 확대, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181836. 그림 확대/그림 확대.py
+++ 프로그래머스/unrated/181836. 그림 확대/그림 확대.py
@@ -1,15 +1,22 @@
 def solution(picture, k):
-    result = []
+#     result = []
     
-    for ele in picture:
-        temp = []
+#     for ele in picture:
+#         temp = []
         
-        for cha in ele:
-            if cha == '.':
-                temp.append('.' * k)
-            elif cha == 'x':
-                temp.append('x' * k)
-        for _ in range(k):
-            result.append(''.join(temp))
+#         for cha in ele:
+#             if cha == '.':
+#                 temp.append('.' * k)
+#             elif cha == 'x':
+#                 temp.append('x' * k)
+#         for _ in range(k):
+#             result.append(''.join(temp))
             
-    return result
+#     return result
+
+    answer = []
+    
+    for i in range(len(picture)):
+        for _ in range(k):
+            answer.append(picture[i].replace('.','.' * k).replace('x','x' * k))
+    return answer
