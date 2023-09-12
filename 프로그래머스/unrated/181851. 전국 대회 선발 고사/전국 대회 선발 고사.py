5415bb1e710edab8223d73f7f0626a0ed5600ceb
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-13 00:18:02 +0900

    Update 전국 대회 선발 고사.py

--- 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
@@ -25,3 +25,8 @@ def solution(rank, attendance):
 총 3명을 찾으면 종료한다.
 '''
         
+def solution(rank, attendance):
+    
+    arr = sorted([(r, num) for (num, r) in enumerate(rank) if attendance[num]])
+    #[ (몇등, 몇번)]
+    return 10000 * arr[0][1] + 100 * arr[1][1] + arr[2][1]
