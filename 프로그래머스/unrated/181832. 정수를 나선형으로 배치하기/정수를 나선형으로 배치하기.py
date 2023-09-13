bb51d3d97275028855e1a5a5652a2734d95ad605
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-14 01:27:58 +0900

    [unrated] Title: 정수를 나선형으로 배치하기, Time: 0.27 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
+++ 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
@@ -1,32 +1,21 @@
 def solution(n):
-    array = [[0] * n for _ in range(n)]
-
-    count = 1
-
-    startRow = 0
-    endRow = n - 1
-    startCol = 0
-    endCol = n - 1
-
-    while count <= n * n:
-        for i in range(startCol, endCol + 1):
-            array[startRow][i] = count
-            count += 1
-        startRow += 1
-
-        for i in range(startRow, endCol + 1):
-            array[i][endCol] = count
-            count += 1
-        endCol -= 1
-
-        for i in range(endCol, startCol - 1, -1):
-            array[endRow][i] = count
-            count += 1
-        endRow -= 1
-
-        for i in range(endRow, startRow - 1, -1):
-            array[i][startCol] = count
-            count += 1
-        startCol += 1
-
-    return array
+    dr = 0
+    di = [0, 1, 0, -1]
+    dj = [1, 0, -1, 0]
+    
+    arr = [[0] * n for _ in range(n)]
+    
+    i, j, cnt = 0, 0, 1
+    
+    arr[i][j] = cnt
+    
+    while cnt < n * n:
+        ni, nj = i + di[dr], j + dj[dr]
+        if 0 <= ni < n and 0 <= nj < n and arr[ni][nj] == 0:
+            i, j = ni, nj
+            cnt += 1
+            arr[i][j] = cnt
+        else:
+            dr = (dr + 1) % 4
+        
+    return arr
