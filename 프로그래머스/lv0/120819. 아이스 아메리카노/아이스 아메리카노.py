724b082dc52fd102a82a65099b7da5e5669fc187
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-15 01:01:46 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.py
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.py
@@ -1,5 +1,8 @@
 def solution(money):
-    answer = []
-    answer.append(money // 5500)
-    answer.append(money % 5500)
+    # answer = []
+    # answer.append(money // 5500)
+    # answer.append(money % 5500)
+    # return answer
+    
+    answer = [money // 5500, money % 5500]
     return answer
