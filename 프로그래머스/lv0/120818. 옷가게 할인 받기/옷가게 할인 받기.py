4d7262511ade015ae2d2fe31e440ceb0d889c4c0
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-15 02:42:55 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -1,10 +1,17 @@
 def solution(price):
     
-    if price >= 500000:
-        return int(price * 0.8)
-    elif price >= 300000:
-        return int(price * 0.9)
-    elif price >= 100000:
-        return int(price * 0.95)
-    else:
-        return price
+#     if price >= 500000:
+#         return int(price * 0.8)
+#     elif price >= 300000:
+#         return int(price * 0.9)
+#     elif price >= 100000:
+#         return int(price * 0.95)
+#     else:
+#         return price
+    
+    #
+    table = {500000: 0.8, 300000: 0.9, 100000: 0.95, 0: 1}
+    
+    for p, rate in table.items():
+        if price >= p:
+            return int(price * rate)
