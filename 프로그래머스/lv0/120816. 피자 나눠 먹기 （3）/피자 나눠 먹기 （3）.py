522638d3fcfa486cfc884a6d6e74c6d3c885f072
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-15 02:13:13 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
@@ -1,6 +1,11 @@
 def solution(slice, n):
-    sol = 1
-    while slice * sol < n:
-        sol += 1
+#     sol = 1
+#     while slice * sol < n:
+#         sol += 1
     
-    return sol
+#     return sol
+
+    if n % slice == 0:
+        return n // slice
+    else:
+        return n // slice + 1
