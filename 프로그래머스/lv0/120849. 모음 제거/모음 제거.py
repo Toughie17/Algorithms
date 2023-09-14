88cddc584ed88818f6efc9fd3aabfd57e0c7a508
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-15 02:25:13 +0900

    [level 0] Title: 모음 제거, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.py
@@ -1,3 +1,4 @@
 def solution(my_string):
     vowel = ['a', 'e', 'i', 'o', 'u']
-    return ''.join([a for a in my_string if a not in vowel])
+    # return ''.join([a for a in my_string if a not in vowel])
+    return ''.join([a for a in my_string if not(a in 'aeiou')])
