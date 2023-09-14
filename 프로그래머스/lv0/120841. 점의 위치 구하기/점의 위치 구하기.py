6f21a14444d179cb81d3b88413321ce8646a2dc1
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-15 01:12:28 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.py
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.py
@@ -1,13 +1,21 @@
 def solution(dot):
-    # [양수/음수]
-    x = dot[0]
-    y = dot[1]
+#     # [양수/음수]
+#     x = dot[0]
+#     y = dot[1]
     
-    if x > 0 and y >0:
-        return 1
-    elif x < 0 and y > 0:
-        return 2
-    elif x < 0 and y < 0:
-        return 3
-    elif x > 0 and y < 0:
-        return 4
+#     if x > 0 and y >0:
+#         return 1
+#     elif x < 0 and y > 0:
+#         return 2
+#     elif x < 0 and y < 0:
+#         return 3
+#     elif x > 0 and y < 0:
+#         return 4
+    
+    
+    x, y = dot
+    
+    if x * y > 0:
+        return 1 if x > 0 else 3
+    if x * y < 0:
+        return 2 if x < 0 else 4
