db1521b997e19926eb5db0d61624d7e1b3e25954
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-15 01:14:46 +0900

    [level 0] Title: 배열의 유사도, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.py
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.py
@@ -1,6 +1,10 @@
 def solution(s1, s2):
-    sol = 0
-    for num in s1:
-        if num in s2:
-            sol += 1
-    return sol
+    # sol = 0
+    # for num in s1:
+    #     if num in s2:
+    #         sol += 1
+    # return sol
+
+#교집합으로 생각하면..
+
+    return len(set(s1) & set(s2))
