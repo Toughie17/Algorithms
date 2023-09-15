c960f1ef2239dcc9f89331bb94a19308df751049
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-16 00:23:02 +0900

    [level 0] Title: 주사위의 개수, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.py
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.py
@@ -1,5 +1,8 @@
 def solution(box, n):
-    result = 1
-    for l in box:
-        result *= l // n
-    return result
+    # result = 1
+    # for l in box:
+    #     result *= l // n
+    # return result
+
+    x, y, z = box
+    return (x // n) * (y // n) * (z // n)
