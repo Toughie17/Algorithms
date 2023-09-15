385a16ff73d6287f182be97c27003b7b072631fe
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-16 00:16:16 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -1,8 +1,10 @@
 def solution(numbers):
     #0, 음수, 양수
-    result = []
-    for i in range(len(numbers)):
-        for j in range(i + 1, len(numbers)):
-            result.append(numbers[i] * numbers[j])
+    # result = []
+    # for i in range(len(numbers)):
+    #     for j in range(i + 1, len(numbers)):
+    #         result.append(numbers[i] * numbers[j])
             
-    return max(result)
+    # 
+    sort = sorted(numbers)
+    return max(sort[0] * sort[1], sort[-1] * sort[-2])
