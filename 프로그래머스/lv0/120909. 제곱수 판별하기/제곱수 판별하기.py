991514024e880253fbe7f79f68ee694ad43e0e04
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-15 22:20:43 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.py
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.py
@@ -1,6 +1,8 @@
 def solution(n):
-    sqr = int(n ** 0.5)
+#     sqr = int(n ** 0.5)
     
-    if sqr * sqr == n:
-        return 1
-    return 2
+#     if sqr * sqr == n:
+#         return 1
+#     return 2
+    #정수인지 판별. is_integer()
+    return 1 if (n ** 0.5).is_integer() else 2
