3404afb880533611b056103e4df6b47097deaa9f
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-15 22:48:33 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.py
@@ -1,13 +1,18 @@
 def solution(rsp):
-    answer = []
+#     answer = []
     
-    #가위: 2, 바위:0, 보:5
-    for enemy in rsp:
-        if enemy == '2':
-            answer.append(0)
-        elif enemy == '0':
-            answer.append(5)
-        elif enemy == '5':
-            answer.append(2)
+#     #가위: 2, 바위:0, 보:5
+#     for enemy in rsp:
+#         if enemy == '2':
+#             answer.append(0)
+#         elif enemy == '0':
+#             answer.append(5)
+#         elif enemy == '5':
+#             answer.append(2)
             
-    return ''.join(map(str,answer))
+#     return ''.join(map(str,answer))
+
+
+    #딕셔너리 활용
+    rule = {'2': '0', '0': '5', '5': '2'}
+    return ''.join([rule[i] for i in rsp])
