772fc2c7b5b12d2d344a1ccf0a7b638dd143c40d
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-18 02:59:04 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -0,0 +1,9 @@
+def solution(my_string):
+    
+    #문자열은 immutable, replace하면 새로운 문자열이 생성됨
+    for i in my_string:
+        #알파벳인지 판별
+        if i.isalpha():
+            my_string = my_string.replace(i, ' ')
+            
+    return sum(map(int, my_string.split()))
