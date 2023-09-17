dd928d50c705a20e918f0368951fd5aa670b9b9d
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-18 02:30:57 +0900

    [level 0] Title: A로 B 만들기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.py
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.py
@@ -1,8 +1,11 @@
 def solution(before, after):
-    sol = list(before)
+#     sol = list(before)
     
-    for cha in after:
-        if cha in sol:
-            sol.remove(cha)
+#     for cha in after:
+#         if cha in sol:
+#             sol.remove(cha)
             
-    return 1 if not sol else 0
+#     return 1 if not sol else 0
+
+    return 1 if sorted(before) == sorted(after) else 0
+    # zzz
