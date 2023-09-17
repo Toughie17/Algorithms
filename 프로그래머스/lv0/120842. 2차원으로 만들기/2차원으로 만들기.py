a7f74370103d645f44e011de9c59ab73437355dc
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-18 01:25:15 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.py
@@ -0,0 +1,14 @@
+def solution(num_list, n):
+    row = len(num_list) // n
+    answer = []
+    for i in range(row):
+        temp = []
+        for num in num_list:
+            if len(temp) < n:
+                temp.append(num)
+        answer.append(temp)
+        num_list  = num_list[n:]
+        
+    return answer
+            
+        
