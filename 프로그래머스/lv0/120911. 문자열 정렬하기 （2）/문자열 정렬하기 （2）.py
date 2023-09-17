de09bc170816a4abf66c7a40772a0ceb78bf6a3c
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-18 00:41:47 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.01 ms, Memory: 9.94 MB -BaekjoonHub

--- 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.py
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.py
@@ -1,2 +1,4 @@
 def solution(my_string):
-    return ''.join(sorted([alpha.lower() for alpha in my_string]))
+    # return ''.join(sorted([alpha.lower() for alpha in my_string]))
+    # 이미 스트링 타입은 iterable이니까 ㅎ..
+    return ''.join(sorted(my_string.lower()))
