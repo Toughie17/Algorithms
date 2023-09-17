b287a779a2a2db7c8b993b2b6dab2133e4a0c682
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-18 01:04:43 +0900

    [level 0] Title: 합성수 찾기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.py
@@ -1,7 +1,7 @@
 def solution(n):
     count = 0
     num = []
-    for i in range(1, n+1):
+    for i in range(4, n+1):
         for j in range(1, i+1):
             if i % j == 0:
                 num.append(i)
