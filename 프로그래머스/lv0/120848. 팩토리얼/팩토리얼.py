8a32a2ba6adef421fdd274dc3ca1e5c08afe6996
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-18 02:12:37 +0900

    [level 0] Title: 팩토리얼, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.py
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.py
@@ -1,13 +1,22 @@
-def solution(n):
-    sol = 1
+# def solution(n):
+#     sol = 1
     
-    while True:
-        result = 1
+#     while True:
+#         result = 1
         
-        for i in range(1, sol + 1):
-            result *= i
+#         for i in range(1, sol + 1):
+#             result *= i
             
-        if result > n:
-            return sol - 1
-        else:
-            sol += 1
+#         if result > n:
+#             return sol - 1
+#         else:
+#             sol += 1
+
+from math import factorial
+    
+def solution(n):
+    k = 10
+    while n < factorial(k):
+        k -= 1
+    return k
+        
