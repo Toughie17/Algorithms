5a0d4258628d1360e209da7311e97ce049dede8e
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-18 01:27:56 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.py
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.py
@@ -1,14 +1,17 @@
 def solution(num_list, n):
-    row = len(num_list) // n
-    answer = []
-    for i in range(row):
-        temp = []
-        for num in num_list:
-            if len(temp) < n:
-                temp.append(num)
-        answer.append(temp)
-        num_list  = num_list[n:]
+#     row = len(num_list) // n
+#     answer = []
+#     for i in range(row):
+#         temp = []
+#         for num in num_list:
+#             if len(temp) < n:
+#                 temp.append(num)
+#         answer.append(temp)
+#         num_list  = num_list[n:]
         
+#     return answer
+    answer = []
+    for i in range(0, len(num_list), n):
+        answer.append(num_list[i:i+n])
     return answer
-            
         
