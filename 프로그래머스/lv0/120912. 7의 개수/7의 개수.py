2b0f5263d49c9dd9bed167918d93568cd475eaba
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-18 23:40:30 +0900

    [level 0] Title: 7의 개수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120912. 7의 개수/7의 개수.py
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.py
@@ -1,8 +1,12 @@
 def solution(array):
-    count = 0
+#     count = 0
     
-    for num in array:
-        for ele in str(num):
-            if ele == '7':
-                count += 1
-    return count
+#     for num in array:
+#         for ele in str(num):
+#             if ele == '7':
+#                 count += 1
+#     return count
+
+    # return str(array).count('7')
+
+    return ''.join(map(str,array)).count('7')
