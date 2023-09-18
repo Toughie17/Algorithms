a37dc5e20820011970db4cd9815b984d68833162
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-18 22:58:53 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
@@ -1,12 +1,15 @@
 def solution(s):
-    counts = {}
+#     counts = {}
     
-    for cha in s:
-        if cha in counts:
-            counts[cha] += 1
-        else:
-            counts[cha] = 1
+#     for cha in s:
+#         if cha in counts:
+#             counts[cha] += 1
+#         else:
+#             counts[cha] = 1
     
-    uniques = [cha for cha, count in counts.items() if count == 1]
+#     uniques = [cha for cha, count in counts.items() if count == 1]
     
-    return ''.join(sorted(uniques))
+#     return ''.join(sorted(uniques))
+
+    #카운트를 왤케 까먹지
+    return ''.join(sorted([cha for cha in s if s.count(cha) == 1]))
