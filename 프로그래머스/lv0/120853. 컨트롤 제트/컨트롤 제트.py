56f338ecffc2d54e1d5f27cbc3971c54d684e5c7
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-18 23:29:12 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.py
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.py
@@ -1,14 +1,24 @@
 def solution(s):
-    eles = s.split()
-    result = 0
+#     eles = s.split()
+#     result = 0
     
-    for index, ele in enumerate(eles):
-        if ele == 'Z':
-            prev = int(eles[index - 1])
-            result -= prev
-        else:
-            result += int(eles[index])
+#     for index, ele in enumerate(eles):
+#         if ele == 'Z':
+#             prev = int(eles[index - 1])
+#             result -= prev
+#         else:
+#             result += int(eles[index])
             
-    return result
+#     return result
+
+    #스택 활용
+    stack = []
+    for ele in s.split():
+        if ele != 'Z':
+            stack.append(int(ele))
+        else:
+            stack.pop()
+            
+    return sum(stack)
     
     
