22cd1a2720ed55d3956c421a6ac8bcae7ad429fd
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-18 23:55:01 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -1,27 +1,30 @@
 def solution(numbers):
     #이걸 어떻게 쪼개지
     #쪼개는게 아니라 키값이 될 때까지 순회하는 거구나
-    wordToNum = {
-        'zero': '0',
-        'one': '1',
-        'two': '2',
-        'three': '3',
-        'four': '4',
-        'five': '5',
-        'six': '6',
-        'seven': '7',
-        'eight': '8',
-        'nine': '9'
-    }
+#     wordToNum = {
+#         'zero': '0',
+#         'one': '1',
+#         'two': '2',
+#         'three': '3',
+#         'four': '4',
+#         'five': '5',
+#         'six': '6',
+#         'seven': '7',
+#         'eight': '8',
+#         'nine': '9'
+#     }
     
-    result = ''
-    current = ''
+#     result = ''
+#     current = ''
     
-    for cha in numbers:
-        current += cha
-        if current in wordToNum:
-            result += wordToNum[current]
-            current = ''
+#     for cha in numbers:
+#         current += cha
+#         if current in wordToNum:
+#             result += wordToNum[current]
+#             current = ''
             
-    return int(result)
-    
+#     return int(result)
+
+    for index, eng in enumerate(["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]):
+        numbers = numbers.replace(eng, str(index))
+    return int(numbers)
