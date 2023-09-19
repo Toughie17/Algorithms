954684b8d868a7692005c69cb7c62bdb879076e9
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-20 01:15:50 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
@@ -0,0 +1,17 @@
+def solution(sides):
+    default = sorted(sides)[0]
+    default_long = sorted(sides)[1]
+    new_long = default_long + 1
+    answer = []
+    
+    #가장 긴 변이 기본인 경우
+    for i in range(1, default_long + 1):
+        if default + i > default_long:
+            answer.append(i)
+    #가장 긴 변의 가능성
+    while default + default_long > new_long:
+        answer.append(new_long)
+        new_long += 1
+        
+    return len(answer)
+        
