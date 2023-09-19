ea334e9c8017e67449168860a4b11ffd4c0c8841
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-20 00:33:20 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.py
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -1,5 +1,8 @@
 def solution(my_string):
-    return eval(my_string)
+    # return eval(my_string)
+    return sum(map(int,my_string.replace(' - ', ' + -').split('+')))
+    # "3 + 4 - 7 - 8"
+    # "3 + 4 + -7 + -8"
 
 
     
