4bcbe1307b34724238a3bd8decf5e2d56d406af4
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-22 01:31:50 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 9.89 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.py
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.py
@@ -1,8 +1,7 @@
 def solution(id_pw, db):
-    
     for info in db:
         if id_pw[0] in info:
             if id_pw[1] == info[1]:
-                return "login"
+                return 'login'
             return 'wrong pw'
     return 'fail'
