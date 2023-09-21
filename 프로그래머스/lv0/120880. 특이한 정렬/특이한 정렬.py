b1d354f8216579d52b1ddcf483ea6847d2a4073e
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-22 03:27:40 +0900

    [level 0] Title: 특이한 정렬, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.py
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.py
@@ -1,4 +1,6 @@
 def solution(numlist,n):
-    distance = [(abs(n - x), x) for x in numlist]
-    distance.sort(key= lambda x: (x[0], -x[1]))
-    return [x[1] for x in distance]
+    # distance = [(abs(n - x), x) for x in numlist]
+    # distance.sort(key= lambda x: (x[0], -x[1]))
+    # return [x[1] for x in distance]
+    
+    return sorted(numlist, key = lambda x: (abs(n - x), n - x))
