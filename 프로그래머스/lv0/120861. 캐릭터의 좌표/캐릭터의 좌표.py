9ed6a842ba36bd80ac1e57d71c682ef235c30dd1
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-22 00:39:18 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -1,21 +1,35 @@
 def solution(keyinput, board):
-    x = 0
-    y = 0
+#     x = 0
+#     y = 0
     
-    x_limit = (board[0] - 1) / 2
-    y_limit = (board[1] - 1) / 2
+#     x_limit = (board[0] - 1) / 2
+#     y_limit = (board[1] - 1) / 2
     
-    inputs = {'up': 1, 'down': -1, 'left': -1, 'right': 1}
+#     inputs = {'up': 1, 'down': -1, 'left': -1, 'right': 1}
+    
+#     for input in keyinput:
+#         if input == 'up' or input == 'down':
+#             new_y = y + inputs[input]
+#             if -y_limit <= new_y <= y_limit:
+#                 y = new_y
+                
+#         elif input == 'left' or input == 'right':
+#             new_x = x + inputs[input]
+#             if -x_limit <= new_x <= x_limit:
+#                 x = new_x
+    
+#     return [x, y]
+    x,y = 0,0
+    xlim,ylim = board[0] // 2, board[1] // 2
+    move = {'up': (0,1), 'down': (0,-1), 'left': (-1,0), 'right': (1,0)}
     
     for input in keyinput:
-        if input == 'up' or input == 'down':
-            new_y = y + inputs[input]
-            if -y_limit <= new_y <= y_limit:
-                y = new_y
-                
-        elif input == 'left' or input == 'right':
-            new_x = x + inputs[input]
-            if -x_limit <= new_x <= x_limit:
-                x = new_x
-    
+        dx, dy = move[input]
+        
+        if abs(x + dx) > xlim or abs(y + dy) > ylim:
+            continue
+        else:
+            x += dx
+            y += dy
+            
     return [x, y]
