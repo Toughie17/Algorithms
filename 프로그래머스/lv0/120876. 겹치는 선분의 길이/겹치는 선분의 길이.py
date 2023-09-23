83999629781018a75125e9c607ad52056cc1898e
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-24 00:46:10 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
@@ -1,3 +1,6 @@
 def solution(lines):
+    #끝점과 시작점이 같으면 선분이 겹치는 것이 아님. 교집합으로 처리하면 안 됨.
+    #그래서 그냥 꼬리를 잘라서 처리
     sets = [set(range(min(l), max(l))) for l in lines]
+    #각 선분의 교집합의 길이를 구한다(숫자 카운팅) 그리고 합집합을 통해 중복을 제거한다.
     return len(sets[0] & sets[1] | sets[0] & sets[2] | sets[1] & sets[2])
