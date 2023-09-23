f008863b324c750a02a646a4c576a73030a7140e
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-23 22:26:19 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.py
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -1,6 +1,12 @@
 def solution(data):
-    
-    if data[1] - data[0] == data[2] - data[1]:
-        return data[-1] + data[1] - data[0]
-    else:
-        return data[-1] * (data[1] / data[0])
+#     if data[1] - data[0] == data[2] - data[1]:
+#         return data[-1] + data[1] - data[0]
+#     else:
+#         return data[-1] * (data[1] / data[0])
+
+    #common의 길이가 2 초과니까 최소 원소는 3개 이상이다.
+    a, b, c = data[:3]
+    if (b-a) == (c-b):
+        return data[-1] + (b-a)
+    elif (b/a) == (c/b):
+        return data[-1] * (b/a)
