b8ec6a572d995d435ff49eda2f001e7e8b9c1a9b
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-23 18:50:12 +0900

    [level 0] Title: OX퀴즈, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.py
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.py
@@ -1,10 +1,21 @@
 def solution(quiz):
     answer = []
     
+    # for q in quiz:
+    #     splited = q.split(' = ')
+    #     if eval(splited[0]) == int(splited[1]):
+    #         answer.append('O')
+    #     else:
+    #         answer.append('X')
+    # return answer
+
     for q in quiz:
-        splited = q.split(' = ')
-        if eval(splited[0]) == int(splited[1]):
-            answer.append('O')
+        L,R = q.split(' = ')
+        a,op,b = L.split()
+        
+        if op == '+':
+            answer.append('O') if int(a) + int(b) == int(R) else answer.append('X')
         else:
-            answer.append('X')
+            answer.append('O') if int(a) - int(b) == int(R) else answer.append('X')
+    
     return answer
