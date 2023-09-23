9d9e28c88fcdf99f935bf369beef75679615c28b
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-23 23:12:12 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.py
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.py
@@ -1,10 +1,18 @@
 def solution(num, total):
     #가우스 공식
     #연속합 = n / 2 (2 * 첫 수 + (n - 1)d) 
-    result = []
-    a = (2 * total / num - (num - 1)) / 2
+#     result = []
+#     a = (2 * total / num - (num - 1)) / 2
     
-    for i in range(num):
-        result.append(a + i)
-    return result
+#     for i in range(num):
+#         result.append(a + i)
+#     return result
+
+    if num % 2 == 1:
+        return list(range(total // num - num // 2, total // num + num // 2 + 1 ))
+    else:
+        return list(range(total // num - num // 2 + 1,  total // num + num // 2 + 1))
+
+
+
     
