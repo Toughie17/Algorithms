a160c712b4429c99531e7c93cec789f839541aff
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-24 00:13:47 +0900

    [level 0] Title: 안전지대, Time: 0.09 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.py
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.py
@@ -1,25 +1,25 @@
 def solution(board):
+    #전체 칸 수에서 위험지역 칸 수를 빼서 답을 구한다.
     n = len(board)
+    #지뢰가 인접한 경우 중복되는 위험 지역이 있기 때문에 set 활용
     danger = set()
     
+    #먼저 행을 순회하고
     for i, row in enumerate(board):
-        #3, [0,0,1,0,0]
+        #열을 순회하면서 1을 찾는다.
         for j, x in enumerate(row):
-            #2, 1
+            # 1을 찾자(지뢰 찾기) - 정수의 경우 0이 False 이외는 1임을 활용 
             if x:
                 danger.update(
+                    #몇행 몇열? 
                     (i + di, j + dj)
-                    #위의 행, 자기 행, 아래 행
+                    #위 아래 행 + 지뢰자리
                     for di in [-1, 0, 1]
-                    #각 행에서 왼쪽, 지뢰위치, 오른쪽
+                    #양 옆 열 + 지뢰자리
                     for dj in [-1, 0, 1]
-                    if 0 <= i + di < n and 0 <= j + dj < n
+                    #보드를 벗어나지 않는 경우만
+                    if 0<= i + di < n and 0 <= j + dj < n
                 )
-    
+                
     return n * n - len(danger)
-
-# [0,0,0,0,0]
-# [0,0,0,0,0]
-# [0,x,x,x,0]
-# [0,x,1,x,0]
-# [0,x,x,x,0]
+            
