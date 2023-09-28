dfce01e70d5d984ad4e95a45b4cb775677f307f6
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-28 22:10:28 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.13 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
@@ -1,5 +1,9 @@
 def solution(a, b):
-    if a <= b:
-        return sum([x for x in range(a,b + 1)])
-    elif a > b:
-        return sum([x for x in range(b,a + 1)])
+    # if a <= b:
+    #     return sum([x for x in range(a,b + 1)])
+    # elif a > b:
+    #     return sum([x for x in range(b,a + 1)])
+    
+    if a > b:
+        a, b = b, a
+    return sum(range(a, b+1))
