b54e27409b392c31340587372cccb74279ec75a8
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-09-28 22:31:26 +0900

    [level 1] Title: 음양 더하기, Time: 0.06 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
@@ -1,8 +1,15 @@
 def solution(absolutes, signs):
     answer = 0
     
-    for i, num in enumerate(absolutes):
-        if signs[i]:
+#     for i, num in enumerate(absolutes):
+#         if signs[i]:
+#             answer += num
+#         else:
+#             answer -= num
+#     return answer
+
+    for num, sign in zip(absolutes, signs):
+        if sign:
             answer += num
         else:
             answer -= num
