a28bec50612ef070fd8072ae7d16bc5ee9edfad0
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-10-01 23:18:23 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -1,4 +1,6 @@
 def solution(numbers):
-    all = set(range(10))
-    return sum(all - set(numbers))
+    # all = set(range(10))
+    # return sum(all - set(numbers))
+
+    return sum(num for num in list(range(10)) if num not in numbers)
     
