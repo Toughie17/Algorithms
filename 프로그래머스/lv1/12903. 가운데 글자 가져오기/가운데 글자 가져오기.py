f8352426ab85ad67607f5640f5c2c6d253df6f5f
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-10-03 00:52:59 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
@@ -1,8 +1,11 @@
 def solution(s):
-    c = len(s) // 2
+#     c = len(s) // 2
     
-    if len(s) % 2 == 1:
-        return s[c]
-    else:
-        return s[c-1: c + 1]
+#     if len(s) % 2 == 1:
+#         return s[c]
+#     else:
+#         return s[c-1: c + 1]
+    
+    return s[(len(s) -1) // 2 : len(s) // 2 + 1]
+
     
