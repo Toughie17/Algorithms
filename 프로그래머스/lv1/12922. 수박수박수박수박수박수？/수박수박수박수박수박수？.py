afe77cdba6ab752abcc1884dabbf5624ebdcc7a6
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-10-03 01:01:31 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
@@ -1,12 +1,14 @@
 def solution(n):
-    answer = []
+#     answer = []
     
-    for i in range(1, n + 1):
-        #홀수면 수 짝수면 박
-        if i % 2 == 1:
-            answer.append('수')
-        else:
-            answer.append('박')
+#     for i in range(1, n + 1):
+#         #홀수면 수 짝수면 박
+#         if i % 2 == 1:
+#             answer.append('수')
+#         else:
+#             answer.append('박')
             
-    return ''.join(answer)
+#     return ''.join(answer)
+
+    return '수박' * (n // 2) + '수' * (n % 2)
         
