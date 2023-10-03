f0052302ed7cf7e94c104065bd443af1489eb867
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-10-04 02:02:39 +0900

    [level 1] Title: 행렬의 덧셈, Time: 35.54 ms, Memory: 41.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -1,3 +1,4 @@
+import numpy as np
 def solution(arr1, arr2):
     result = []
     
@@ -10,3 +11,20 @@ def solution(arr1, arr2):
             new.append(arr1[i][j] + arr2[i][j])
         result.append(new)
     return result
+
+    #ZIP 쓰는 방법
+    # answer = [[c + d for c, d in zip(a,b)] for a, b in zip(A,B)]
+    # a, b = ([1,2], [3, 4]) , ([2,3], [5,6])
+    # c, d = ([1,2], [2, 3]) , ([3,4], [5,6])
+    
+    # return answer
+    
+    #넘파이 쓰는 방법
+    # A_np = np.array(arr1)
+    # print(A_np)
+    # B_np = np.array(arr2)
+    # print(B_np)
+    # result = A_np + B_np
+    # print(result)
+    # print(result.tolist())
+    # return result.tolist()
