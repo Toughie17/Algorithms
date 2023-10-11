3e0650c8add9fe41d449fce3ae0cae22c56048ae
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-10-12 00:02:12 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,17 @@
+def solution(s):
+    table = {'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5,
+            'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
+    
+    answer = []
+    temp = ''
+
+    for cha in s:
+        if cha.isdigit():
+            answer.append((cha))
+        else:
+            temp += cha
+            if temp in table:
+                answer.append(str((table[temp])))
+                temp = ''
+    
+    return(int(''.join(answer)))
