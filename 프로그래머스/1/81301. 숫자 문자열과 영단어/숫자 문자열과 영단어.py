62aa03e869dab29131cc25dbbd5f36c6cdc76e91
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-10-12 00:13:14 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,17 +1,23 @@
 def solution(s):
-    table = {'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5,
-            'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
+#     table = {'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5,
+#             'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
     
-    answer = []
-    temp = ''
+#     answer = []
+#     temp = ''
 
-    for cha in s:
-        if cha.isdigit():
-            answer.append((cha))
-        else:
-            temp += cha
-            if temp in table:
-                answer.append(str((table[temp])))
-                temp = ''
+#     for cha in s:
+#         if cha.isdigit():
+#             answer.append((cha))
+#         else:
+#             temp += cha
+#             if temp in table:
+#                 answer.append(str((table[temp])))
+#                 temp = ''
     
-    return(int(''.join(answer)))
+#     return(int(''.join(answer)))
+
+    table = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    
+    for i in range(len(table)):
+        s = s.replace(table[i], str(i))
+    return int(s)
