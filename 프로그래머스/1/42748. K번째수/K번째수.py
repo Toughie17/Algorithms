e6c8e9b335acab0695a832e178bdb577bbf343c2
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-10-15 18:53:46 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,9 +1,11 @@
 def solution(array, commands):
 
-    answer = []
+#     answer = []
 
-    for com in commands:
-        part = array[com[0] - 1: com[1]]
-        answer.append(sorted(part)[com[2] - 1])
+#     for com in commands:
+#         part = array[com[0] - 1: com[1]]
+#         answer.append(sorted(part)[com[2] - 1])
     
-    return answer
+#     return answer
+
+    return [sorted(array[com[0] -1: com[1]])[com[2]-1] for com in commands]
