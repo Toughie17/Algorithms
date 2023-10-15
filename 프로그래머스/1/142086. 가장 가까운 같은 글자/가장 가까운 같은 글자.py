6325513462337bf44ebd96337f925255ccd895ba
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2023-10-15 17:42:50 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 3.68 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -1,14 +1,27 @@
 def solution(s):
-    test = []
-    answer = []
+#     test = []
+#     answer = []
 
-    for cha in s:
-        if cha in test:
-            test.append(cha)
-            idx = ([index for index, value in enumerate(test) if value == cha])
-            answer.append(idx[-1] - idx[-2])
-        else:
-            test.append(cha)
-            answer.append(-1)
+#     for cha in s:
+#         if cha in test:
+#             test.append(cha)
+#             idx = ([index for index, value in enumerate(test) if value == cha])
+#             answer.append(idx[-1] - idx[-2])
+#         else:
+#             test.append(cha)
+#             answer.append(-1)
             
-    return answer
+#     return answer
+    
+    answer = []
+    dic = dict()
+    
+    for i in range(len(s)):
+        if s[i] not in dic:
+            answer.append(-1)
+        else:
+            answer.append(i - dic[s[i]])
+        
+        dic[s[i]] = i
+    
+    return(answer)
