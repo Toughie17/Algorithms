429eb85550d9a011ce663df90e1334b2595b874e
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2024-01-30 13:29:18 +0900

    [level 4] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -1,15 +1,44 @@
-select c.car_id, c.car_type, (c.daily_fee*((100-d.DISCOUNT_RATE)/100))*30 as fee
-from (
-    SELECT a.car_id, a.car_type, a.daily_fee
-    from CAR_RENTAL_COMPANY_CAR a
-    where a.car_id not in (
-    select car_id from CAR_RENTAL_COMPANY_RENTAL_HISTORY
-    where to_char(START_DATE, 'YYYYMMDD') <= '20221130' and to_char(END_DATE, 'YYYYMMDD') >= '20221101'
+-- select 
+-- c.car_id, c.car_type, (c.daily_fee*((100-d.DISCOUNT_RATE)/100))*30 as fee
+-- from (
+--     SELECT a.car_id, a.car_type, a.daily_fee
+--     from CAR_RENTAL_COMPANY_CAR a
+--     where a.car_id not in (
+--     select car_id from CAR_RENTAL_COMPANY_RENTAL_HISTORY
+--     where to_char(START_DATE, 'YYYYMMDD') <= '20221130' and to_char(END_DATE, 'YYYYMMDD') >= '20221101'
+--     )
+--     and a.CAR_TYPE in ('세단','SUV') 
+-- ) c
+
+-- ,CAR_RENTAL_COMPANY_DISCOUNT_PLAN d
+-- where c.CAR_TYPE = d.CAR_TYPE
+-- and d.DURATION_TYPE LIKE '30%'
+
+-- and (c.daily_fee*((100-d.DISCOUNT_RATE)/100))*30 BETWEEN 500000 and 2000000
+
+-- order by 3 desc, 2, 1 desc
+
+    
+WITH CAR AS (
+SELECT
+    CAR_ID, CAR_TYPE, DAILY_FEE
+    FROM CAR_RENTAL_COMPANY_CAR 
+    WHERE CAR_ID NOT IN (
+    SELECT CAR_ID FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY 
+        WHERE TO_CHAR(START_DATE,'YYYY-MM-DD') <= '2022-11-30'
+        AND TO_CHAR(END_DATE, 'YYYY-MM-DD') >='2022-11-01'
     )
-    and a.CAR_TYPE in ('세단','SUV') 
-) c
-,CAR_RENTAL_COMPANY_DISCOUNT_PLAN d
-where c.CAR_TYPE = d.CAR_TYPE
-and d.DURATION_TYPE LIKE '30%'
-and (c.daily_fee*((100-d.DISCOUNT_RATE)/100))*30 BETWEEN 500000 and 2000000
-order by 3 desc, 2, 1 desc
+    AND CAR_TYPE IN ('세단', 'SUV')
+)
+
+SELECT 
+    C.CAR_ID,
+    C.CAR_TYPE, 
+    ROUND(C.DAILY_FEE * 30 * (1-(P.DISCOUNT_RATE/100))) FEE
+FROM CAR C
+JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN P
+ON C.CAR_TYPE = P.CAR_TYPE
+WHERE P.DURATION_TYPE = '30일 이상' 
+AND ROUND(C.DAILY_FEE * 30 * (1-(P.DISCOUNT_RATE/100))) >= 500000 
+AND ROUND(C.DAILY_FEE * 30 * (1-(P.DISCOUNT_RATE/100))) < 2000000
+ORDER BY FEE DESC, C.CAR_TYPE ASC, C.CAR_ID DESC;
