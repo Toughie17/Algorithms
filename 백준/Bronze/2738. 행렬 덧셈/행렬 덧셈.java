9ae0000f663013bcfe1de0eca41e068f267ea289
ByungJun Kim <99641242+Toughie17@users.noreply.github.com>
2024-03-07 13:25:41 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 1084 ms, Memory: 47512 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+        int col = sc.nextInt();
+
+        int[] aCol = new int[row*col];
+        int[] bCol = new int[row*col];
+
+        for(int i = 0; i < row*col; i++) {
+            aCol[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < row*col; i++) {
+            bCol[i] = sc.nextInt();
+        }
+
+        for (int i =0; i< row*col; i+=col) {
+            for (int j =0; j < col; j++) {
+                System.out.printf("%d ", aCol[i +j] + bCol[i +j]);
+            }
+            System.out.println();
+        }
+    }
+}
